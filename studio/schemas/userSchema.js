fdd35f6d3d7744715697303871bacd49ea9b5fbf
Zainab Oufqir <zayna.oufkir@gmail.com>
2022-10-15 01:31:52 +0100

    setup sanity and client header

---
+++ studio/schemas/userSchema.js
@@ -0,0 +1,30 @@
+
+export const  userSchema = {
+    name: 'users',
+    title: 'Users',
+    type: 'document',
+    fields:[
+        {
+            name: "address",
+            title: "Wallet Address",
+            type: "string"
+        },
+        {
+            name: "username",
+            title: "User name",
+            type: "string"
+        },
+        {
+            name: "transactions",
+            title: "Transactions",
+            type: "array",
+            of: [
+                {
+                    type: 'reference',
+                    to: [{ type: 'transactions' }]
+                }
+            ]
+        },
+
+    ]
+}
