6c2cb957dbee3b97466d1b17613c75f93a724786
Zainab Oufqir <zayna.oufkir@gmail.com>
2022-10-15 01:59:50 +0100

    setup form

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Header from './../components/Header'
+import Main from './../components/Main'
 
 const style = {
   wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select none flex flex-col justify between`
@@ -9,6 +10,7 @@ const Home: NextPage = () => {
   return (
     <div className={style.wrapper}>
       <Header />
+      <Main />
     </div>
   )
 }
